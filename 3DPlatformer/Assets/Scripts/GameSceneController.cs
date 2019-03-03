3fba34d70aa2c1706e3ee662c6d1cb39192dfa6c
ken <heyty005@unisa.edu.au>
2019-03-04 00:05:45 +1030

    upload

---
+++ 3DPlatformer/Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameSceneController : MonoBehaviour {
+    public Camera gameCamera;
+    public PlayerController player;
+    public Text gameOverText;
+    public Text winText;
+	// Use this for initialization
+	void Start () {
+        gameOverText.enabled = false;
+        winText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (player != null)
+        {
+            gameCamera.transform.position = new Vector3(
+                Mathf.Lerp(gameCamera.transform.position.x, player.transform.position.x, Time.deltaTime * 2),
+                Mathf.Lerp(gameCamera.transform.position.y, player.transform.position.y, Time.deltaTime * 2),
+                gameCamera.transform.position.z
+                );
+            if(player.GetComponent<Rigidbody>().useGravity == false)
+            {
+                winText.enabled = true;
+                winText.text = "You Win! press R to play again";
+                if (Input.GetKeyDown(KeyCode.R))
+                    Restart();
+            }
+            
+        }
+        else if(player == null)
+        {
+            gameOverText.enabled = true;
+            gameOverText.text = "Game over! press R to restart";
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Restart();
+            }
+        }
+      
+	}
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    
+}
---
+++ 3DPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float jumpSpeed;
+    
+
+    Rigidbody rb;
+    bool isJumping;
+    bool facingRight;
+    
+	// Use this for initialization
+	void Start () {
+        facingRight = true;
+        isJumping = false;
+        rb = GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        ProcessInput();
+       
+	}
+
+    void ProcessInput() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
+        if (Input.GetKey(KeyCode.D))
+            MoveRight();
+     
+        if (Input.GetKey(KeyCode.A))
+            MoveLeft();
+    }
+
+    void Jump()
+    {
+        if (isJumping == false) { 
+            rb.AddForce(Vector3.up * jumpSpeed);
+        }
+       
+        isJumping = true;
+    }
+
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+
+    void MoveRight()
+    {
+        facingRight = true;
+        rb.velocity = new Vector3(
+              speed * Time.deltaTime,
+              rb.velocity.y,
+              rb.velocity.z
+              );
+    }
+
+    void MoveLeft()
+    {
+        facingRight = false;
+        rb.velocity = new Vector3(
+              -speed * Time.deltaTime,
+              rb.velocity.y,
+              rb.velocity.z
+              );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ObsticleController>() != null)
+            Destroy(gameObject);
+        if(other.gameObject.tag == "WinPoint")
+        {
+            Win();
+        }
+    }
+
+    public void Win()
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+}
